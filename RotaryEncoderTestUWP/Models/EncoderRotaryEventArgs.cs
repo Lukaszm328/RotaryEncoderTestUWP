1f316d9d7afd48e5d12d0eb5f6bc70ba0c693b6b
Lukasz Kucia <lukaszmpakietm3@gmail.com>
2020-04-19 13:57:36 +0200

    Add RotaryEncoder model

---
+++ RotaryEncoderTestUWP/Models/EncoderRotaryEventArgs.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace RotaryEncoderTestUWP.Models
+{
+    public class EncoderRotaryEventArgs : EventArgs
+    {
+        private LastPosition position;
+        public LastPosition Position 
+        {
+            get { return position; }  
+            set { position = value; }
+        }
+
+        private ButtonState buttonState;
+        public ButtonState Button
+        {
+            get { return buttonState; }
+            set { buttonState = value; }
+        }
+
+        public enum LastPosition
+        {
+            Left,
+            Right
+        }
+
+        public enum ButtonState
+        {
+            Pressed,
+            Released
+        }
+    }
+}
---
+++ RotaryEncoderTestUWP/Models/RotaryEncoder.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Windows.Devices.Gpio;
+
+namespace RotaryEncoderTestUWP.Models
+{
+    class RotaryEncoder
+    {
+        private GpioController GPIO;
+        private GpioPin encoderCLK, encoderDT, encoderButton;
+        private GpioPinValue aState;
+        private GpioPinValue aLastState;
+
+        public RotaryEncoder()
+        {
+            GPIO = GpioController.GetDefault();
+
+            encoderCLK.ValueChanged += EncoderCLK_ValueChanged;
+            encoderCLK.DebounceTimeout = TimeSpan.FromTicks(1);
+            
+            encoderDT.ValueChanged += EncoderDT_ValueChanged;
+            encoderDT.DebounceTimeout = TimeSpan.FromTicks(1);
+        }
+
+        /// <summary>
+        /// Open pin for encoder DT, CLK and button.
+        /// </summary>
+        /// <param name="DT"></param>
+        /// <param name="CLK"></param>
+        /// <param name="btn"></param>
+        public void OpenPin(int DT, int CLK, int btn)
+        {
+            encoderCLK = GPIO.OpenPin(CLK);
+            encoderDT = GPIO.OpenPin(DT);
+            encoderButton = GPIO.OpenPin(btn); 
+        }
+
+        private void EncoderDT_ValueChanged(GpioPin sender, GpioPinValueChangedEventArgs args) => EncoderCheckRotary(sender);
+
+        private void EncoderCLK_ValueChanged(GpioPin sender, GpioPinValueChangedEventArgs args) => EncoderCheckRotary(sender);
+
+        /// <summary>
+        /// Event change encoder value (left,right)
+        /// </summary>
+        public event EventHandler<EncoderRotaryEventArgs> ValueChanged;
+
+        private void EncoderCheckRotary(GpioPin gpioPin)
+        {
+            aState = encoderCLK.Read();
+
+            if (aState != aLastState)
+            {
+                if (encoderDT.Read() != aState)
+                {
+                    EncoderValueChange(gpioPin, EncoderRotaryEventArgs.LastPosition.Left);
+                }
+                else
+                {
+                    EncoderValueChange(gpioPin, EncoderRotaryEventArgs.LastPosition.Right);
+                }
+            }
+            aLastState = aState;
+        }
+
+        private void EncoderValueChange(GpioPin gpioPin, EncoderRotaryEventArgs.LastPosition encoderValue)
+        {
+            EncoderRotaryEventArgs args = new EncoderRotaryEventArgs();
+
+            args.Position = encoderValue;
+            args.Button = EncoderRotaryEventArgs.ButtonState.Released;
+
+            ValueChanged.Invoke(gpioPin, args);
+        }
+    }
+}
